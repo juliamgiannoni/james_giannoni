a367cffdddc605eb30f9cd5358452732b891b0ed
Julia Yi <juliamijiyi@gmail.com>
2017-06-03 14:18:46 -0700

    Update image sizing

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,5 +22,6 @@ module JamesGiannoni
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
